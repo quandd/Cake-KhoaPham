18b89db288cefbf2d07a7dd0d67d611aadefe1f8
quandd <kiu.kk147@gmail.com>
2019-06-28 10:47:21 +0700

    Create Middleware For Admin Done

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+            $admin = Auth::user();
+            if($admin->level > 0){
+                return $next($request);
+            }
+            return redirect()->back()->with('note','Ban khong co quyen lam viec nay!');
+    }
+}
