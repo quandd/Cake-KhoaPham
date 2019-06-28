ea86247385f129175bf23aec8396e7cdce5f0150
quandd <kiu.kk147@gmail.com>
2019-06-28 11:36:57 +0700

    Remove Function Delete Category

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -69,7 +69,7 @@
 											</td>
 									<td>
 			                    		<a href="{{asset('admin/category/edit/'.$cate->id)}}" class="btn btn-warning"><span class="glyphicon glyphicon-edit"></span> Sửa</a>
-			                    		<a href="{{asset('admin/category/delete/'.$cate->id)}}" onclick="return confirm('Bạn có chắc chắn muốn xóa?')" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
+{{--	Xoa danh muc san pham		                    		<a href="{{asset('admin/category/delete/'.$cate->id)}}" onclick="return confirm('Bạn có chắc chắn muốn xóa?')" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>--}}
 			                  		</td>
 			                  	</tr>
 			                  	@endforeach
